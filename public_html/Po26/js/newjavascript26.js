8a423dc58764608227d9147d5a0ac192fd1a7ff4
luisg <luisg@LAPTOP-LS84CUBC>
2017-08-02 13:32:18 -0500

    punto 26 terminado

--- public_html/Po26/js/newjavascript26.js
+++ public_html/Po26/js/newjavascript26.js
@@ -1,7 +1,51 @@
-var x = prompt('Write a whole number');
-if (x < 0 ){
-  var result = (x * -1 );
-  console.log('Positive number ' + result );
-} else {
-  console.log('You must enter a number less than zero');
+function option() {
+  var menu = prompt('              1. Make a sum\n\
+              2. Perform a subtraction\n\
+              3. Perform a multiplication \n\
+              4. Make a division \n\
+              0. Quit the program ');
+  return parseInt(menu);
+}
+
+var result = option();
+
+function sum() {
+  var num1 = parseInt(prompt('Say a number'));
+  var num2 = parseInt(prompt('Say a number'));
+  console.log(`the result of the sum is : ${ num1 + num2 }`);
+}
+function subtraction() {
+  var num1 = parseInt(prompt('Say a number'));
+  var num2 = parseInt(prompt('Say a number'));
+  console.log(`the result of the subtraction is : ${ num1 - num2 }`);
+}
+function multiplication() {
+  var num1 = parseInt(prompt('Say a number'));
+  var num2 = parseInt(prompt('Say a number'));
+  console.log(`the result of the multiplication is : ${ num1 * num2 }`);
+}
+function division() {
+  var num1 = parseInt(prompt('Say a number'));
+  var num2 = parseInt(prompt('Say a number'));
+  console.log(`the result of the division is : ${ num1 / num2 }`);
+}
+switch (result) {
+  case 1:
+    sum();
+    break;
+  case 2:
+    subtraction();
+    break;
+  case 3:
+    multiplication();
+    break;
+  case 4:
+    division();
+    break;
+  case 0:
+    console.log('bye');
+    break;
+  default:
+
+    break;
 }
