d802fbc108976a41c78e138ee06311d325eabdf4
luisg <luisg@LAPTOP-LS84CUBC>
2017-08-02 13:30:48 -0500

    punto 12 terminado

---
+++ public_html/Po12/js/newjavascript12.js
@@ -0,0 +1,13 @@
+var x = '     *';
+var i = 0;
+var t = '*';
+var z = ' ';
+while (i <= 5) {
+  if (i === 0) {
+    console.log(`     ${t}`);
+  } else {
+    console.log(x = x.slice(1) + t);
+  }
+  i++;
+
+}
