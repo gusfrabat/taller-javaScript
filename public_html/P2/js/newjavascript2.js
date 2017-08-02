72ff87439200c59331fa9f98bb80703001201746
luisg <luisg@LAPTOP-LS84CUBC>
2017-08-02 13:27:57 -0500

    punto 2 terminado

--- public_html/P2/js/newjavascript2.js
+++ public_html/P2/js/newjavascript2.js
@@ -1,2 +1,2 @@
-var greeting = "Hello, this is my first JavaScript script stored in a variable.";
-console.log (greeting);
+var greeting = " Hello, this is my first JavaScript script stored in a variable. ";
+console.log(greeting);
