feb88c48b87658f5869309ea8960e9496a7f1a82
luisg <luisg@LAPTOP-LS84CUBC>
2017-08-02 13:31:58 -0500

    punto 23 terminado

--- public_html/Po23/js/newjavascript23.js
+++ public_html/Po23/js/newjavascript23.js
@@ -1,7 +1,5 @@
-var x = prompt('Write a whole number');
-if (x < 0 ){
-  var result = (x * -1 );
-  console.log('Positive number ' + result );
-} else {
-  console.log('You must enter a number less than zero');
-}
+var date1 = new Date(prompt('Please enter a date in this format (YYYY,MM,DD)'));
+var date2 = new Date(prompt('Please enter a date in this format (YYYY,MM,DD)'));
+var daysDif = date2.getTime() - date1.getTime();
+var days = Math.round(daysDif / (1000 * 60 * 60 * 24));
+console.log(' DifferenDce between the two dates ' + days / 365 + ' years ' + days / 30 + ' months ' + days + ' days ');
