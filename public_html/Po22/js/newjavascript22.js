88343ba977ba458881289e51d11ec6873810fb34
luisg <luisg@LAPTOP-LS84CUBC>
2017-08-02 13:31:45 -0500

    punto 22 terminado

--- public_html/Po22/js/newjavascript22.js
+++ public_html/Po22/js/newjavascript22.js
@@ -1,7 +1,7 @@
-var x = prompt('Write a whole number');
-if (x < 0 ){
-  var result = (x * -1 );
-  console.log('Positive number ' + result );
-} else {
-  console.log('You must enter a number less than zero');
-}
+	alert ("Program to calculate months, days, minutes and seconds by 15 years ");
+	var month =  12 * 15 ;
+	var day =  365 * 15 ;
+	var hour =  8760 * 15 ;
+	var minute = 525600 * 15 ;
+	var second =  31536000 * 15 ;
+    console.log('in 15 years there are ' + month + ' months ' + day + ' days ' + hour + ' hours ' + minute + ' minutes ' + second + ' seconds ');
