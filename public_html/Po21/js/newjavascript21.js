f2ba8e86eab1bf9c07bf960eec1c7995696945fe
luisg <luisg@LAPTOP-LS84CUBC>
2017-08-02 13:31:35 -0500

    punto 21 terminado

--- public_html/Po21/js/newjavascript21.js
+++ public_html/Po21/js/newjavascript21.js
@@ -1,7 +1,10 @@
-var x = prompt('Write a whole number');
-if (x < 0 ){
-  var result = (x * -1 );
-  console.log('Positive number ' + result );
-} else {
-  console.log('You must enter a number less than zero');
-}
+alert('Program to calculate the hypotenuse of a rectangular triangle\n\
+\n\
+ the hypotenuse is the longest side of a right triangle and is also the side opposite the right angle');
+
+var a = prompt('What is the length of the leg a');
+var b = prompt('What is the length of the leg b');
+var hypotenuse =  Math.hypot(a,b);
+
+console.log(' (a^2+b^2)^(1/2) The hypotenuse of the triangle rectangle is ' + hypotenuse );
+
